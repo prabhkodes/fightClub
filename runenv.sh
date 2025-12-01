28fe0aad5791f8c05d098e07b81d8a37fb31e6c1
J Franco Ray <kean.franco07@gmail.com>
2025-12-01 17:26:58 +0100

    containerization

--- runenv.sh
+++ runenv.sh
@@ -1,2 +1,3 @@
 docker build -t atmosphere-model:latest .
-docker run -it --name atmosphere-model -v $PWD/code/serial:/app atmosphere-model:latest
+docker rm atmosphere-model
+docker run --rm -it --name atmosphere-model -v $PWD/code/serial:/app atmosphere-model:latest
