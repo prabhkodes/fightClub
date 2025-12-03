8a3c807aed03afc7e5b125de2ca1a31087c51c61
Prabhsharan <prabhsharan@dhcp-129-230.wifi.sissa.it>
2025-12-03 11:17:19 +0100

    added pll_io

--- code/serial/module_output.F90
+++ code/serial/module_output.F90
@@ -2,10 +2,9 @@ module module_output
   use mpi
   use calculation_types, only : wp, iowp
   use parallel_parameters, only : i_beg, k_beg
-  ! We need i_start_global and i_end_global to know where to paste data
-  use dimensions, only : nx, nz, nx_global, nprocs, myrank, i_start_global, i_end_global
+  use dimensions, only : nx, nz, nx_global, nprocs, myrank, i_start_global
   use module_types, only : atmospheric_state, reference_state
-  use iodir, only : stderr
+  use iodir, only : stderr, stdout
   use netcdf
 
   implicit none
@@ -16,22 +15,13 @@ module module_output
   public :: write_record
   public :: close_output
 
-  ! Local arrays for calculation
+  ! Local arrays (Small slices)
   real(wp), allocatable, dimension(:,:) :: dens
   real(wp), allocatable, dimension(:,:) :: uwnd
   real(wp), allocatable, dimension(:,:) :: wwnd
   real(wp), allocatable, dimension(:,:) :: theta
 
-  ! Global arrays (Allocated ONLY on Rank 0)
-  real(wp), allocatable, dimension(:,:) :: global_dens
-  real(wp), allocatable, dimension(:,:) :: global_uwnd
-  real(wp), allocatable, dimension(:,:) :: global_wwnd
-  real(wp), allocatable, dimension(:,:) :: global_theta
-
-  ! Arrays for MPI_Gatherv
-  integer, allocatable, dimension(:) :: recvcounts
-  integer, allocatable, dimension(:) :: displs
-
+  ! NetCDF IDs (Only valid on Rank 0)
   integer :: ncid
   integer :: dens_varid, uwnd_varid, wwnd_varid, theta_varid, t_varid
   integer :: rec_out
@@ -41,54 +31,29 @@ module module_output
   subroutine create_output
     implicit none
     integer :: t_dimid, x_dimid, z_dimid
-    integer :: i, ierr, base_nx, remainder
 
-    ! Allocate local arrays
+    ! 1. Allocate LOCAL arrays (Size: local nx, nz)
     allocate(dens(nx,nz))
     allocate(uwnd(nx,nz))
     allocate(wwnd(nx,nz))
     allocate(theta(nx,nz))
 
-    ! Setup for MPI_Gatherv
-    allocate(recvcounts(nprocs))
-    allocate(displs(nprocs))
-
-    ! Calculate receive counts and displacements for each process
-    base_nx = nx_global / nprocs
-    remainder = mod(nx_global, nprocs)
-    
-    displs(1) = 0
-    do i = 1, nprocs
-      if (i-1 < remainder) then
-        recvcounts(i) = (base_nx + 1) * nz
-      else
-        recvcounts(i) = base_nx * nz
-      end if
-      if (i > 1) then
-        displs(i) = displs(i-1) + recvcounts(i-1)
-      end if
-    end do
-
-    ! Only rank 0 creates file and allocates global arrays
+    ! 2. Setup File (RANK 0 ONLY)
     if (myrank == 0) then
-      allocate(global_dens(nx_global,nz))
-      allocate(global_uwnd(nx_global,nz))
-      allocate(global_wwnd(nx_global,nz))
-      allocate(global_theta(nx_global,nz))
+      ! Standard Serial Create (clobber overwrites existing files)
+      call ncwrap(nf90_create('output.nc', nf90_clobber, ncid), __LINE__)
 
-      call ncwrap(nf90_create('output.nc',nf90_clobber,ncid), __LINE__)
-      call ncwrap(nf90_def_dim(ncid,'time',nf90_unlimited,t_dimid), __LINE__)
-      call ncwrap(nf90_def_dim(ncid,'x',nx_global,x_dimid), __LINE__)
-      call ncwrap(nf90_def_dim(ncid,'z',nz,z_dimid), __LINE__)
+      ! Define dimensions using GLOBAL size
+      call ncwrap(nf90_def_dim(ncid, 'time', nf90_unlimited, t_dimid), __LINE__)
+      call ncwrap(nf90_def_dim(ncid, 'x', nx_global, x_dimid), __LINE__) 
+      call ncwrap(nf90_def_dim(ncid, 'z', nz, z_dimid), __LINE__)
+
       call ncwrap(nf90_def_var(ncid,'time',iowp,[t_dimid],t_varid), __LINE__)
-      call ncwrap(nf90_def_var(ncid,'rho',iowp, &
-          [x_dimid,z_dimid,t_dimid],dens_varid), __LINE__)
-      call ncwrap(nf90_def_var(ncid,'u',iowp, &
-          [x_dimid,z_dimid,t_dimid],uwnd_varid), __LINE__)
-      call ncwrap(nf90_def_var(ncid,'w',iowp, &
-          [x_dimid,z_dimid,t_dimid],wwnd_varid), __LINE__)
-      call ncwrap(nf90_def_var(ncid,'theta',iowp, &
-          [x_dimid,z_dimid,t_dimid],theta_varid), __LINE__)
+      call ncwrap(nf90_def_var(ncid,'rho',iowp, [x_dimid,z_dimid,t_dimid],dens_varid), __LINE__)
+      call ncwrap(nf90_def_var(ncid,'u',iowp,   [x_dimid,z_dimid,t_dimid],uwnd_varid), __LINE__)
+      call ncwrap(nf90_def_var(ncid,'w',iowp,   [x_dimid,z_dimid,t_dimid],wwnd_varid), __LINE__)
+      call ncwrap(nf90_def_var(ncid,'theta',iowp,[x_dimid,z_dimid,t_dimid],theta_varid), __LINE__)
+      
       call ncwrap(nf90_enddef(ncid), __LINE__)
     end if
 
@@ -100,14 +65,17 @@ module module_output
     type(atmospheric_state), intent(in) :: atmostat
     type(reference_state), intent(in) :: ref
     real(wp), intent(in) :: etime
-    integer :: i, k, ierr
-    integer, dimension(1) :: st1, ct1
+    integer :: i, k, src, ierr
     integer, dimension(3) :: st3, ct3
-    real(wp), dimension(1) :: etimearr
-    real(wp), allocatable, dimension(:,:) :: temp_buf
+    integer, dimension(1) :: st1, ct1
+    real(wp), dimension(1) :: etime_arr
     integer :: status(MPI_STATUS_SIZE)
+    
+    ! Temp variables for Rank 0 receiving
+    integer :: r_nx, r_istart
+    real(wp), allocatable, dimension(:,:) :: temp_buf
 
-    ! Calculate local fields
+    ! --- 1. Compute Local Fields ---
     do k = 1, nz
       do i = 1, nx
         dens(i,k) = atmostat%dens(i,k)
@@ -118,33 +86,64 @@ module module_output
       end do
     end do
 
-    ! Gather all data to rank 0
-    call MPI_Gatherv(dens, nx*nz, MPI_DOUBLE_PRECISION, &
-                     global_dens, recvcounts, displs, MPI_DOUBLE_PRECISION, &
-                     0, MPI_COMM_WORLD, ierr)
-    call MPI_Gatherv(uwnd, nx*nz, MPI_DOUBLE_PRECISION, &
-                     global_uwnd, recvcounts, displs, MPI_DOUBLE_PRECISION, &
-                     0, MPI_COMM_WORLD, ierr)
-    call MPI_Gatherv(wwnd, nx*nz, MPI_DOUBLE_PRECISION, &
-                     global_wwnd, recvcounts, displs, MPI_DOUBLE_PRECISION, &
-                     0, MPI_COMM_WORLD, ierr)
-    call MPI_Gatherv(theta, nx*nz, MPI_DOUBLE_PRECISION, &
-                     global_theta, recvcounts, displs, MPI_DOUBLE_PRECISION, &
-                     0, MPI_COMM_WORLD, ierr)
-
-    ! Only rank 0 writes to file
+    ! --- 2. Serial I/O Strategy ---
     if (myrank == 0) then
-      st3 = [ 1, 1, rec_out ]
-      ct3 = [ nx_global, nz, 1 ]
-      call ncwrap(nf90_put_var(ncid,dens_varid,global_dens,st3,ct3), __LINE__)
-      call ncwrap(nf90_put_var(ncid,uwnd_varid,global_uwnd,st3,ct3), __LINE__)
-      call ncwrap(nf90_put_var(ncid,wwnd_varid,global_wwnd,st3,ct3), __LINE__)
-      call ncwrap(nf90_put_var(ncid,theta_varid,global_theta,st3,ct3), __LINE__)
+       
+       ! A. Write Time
+       st1 = [ rec_out ]
+       ct1 = [ 1 ]
+       etime_arr(1) = etime
+       call ncwrap(nf90_put_var(ncid, t_varid, etime_arr, start=st1, count=ct1), __LINE__)
 
-      st1 = [ rec_out ]
-      ct1 = [ 1 ]
-      etimearr(1) = etime
-      call ncwrap(nf90_put_var(ncid,t_varid,etimearr,st1,ct1), __LINE__)
+       ! B. Write MY OWN data (Rank 0)
+       st3 = [ i_start_global, 1, rec_out ]
+       ct3 = [ nx, nz, 1 ]
+       call ncwrap(nf90_put_var(ncid, dens_varid, dens, start=st3, count=ct3), __LINE__)
+       call ncwrap(nf90_put_var(ncid, uwnd_varid, uwnd, start=st3, count=ct3), __LINE__)
+       call ncwrap(nf90_put_var(ncid, wwnd_varid, wwnd, start=st3, count=ct3), __LINE__)
+       call ncwrap(nf90_put_var(ncid, theta_varid,theta,start=st3, count=ct3), __LINE__)
+
+       ! C. Receive and Write Workers data ONE BY ONE
+       do src = 1, nprocs - 1
+          ! 1. Receive Dimensions
+          call MPI_Recv(r_nx, 1, MPI_INTEGER, src, 100, MPI_COMM_WORLD, status, ierr)
+          call MPI_Recv(r_istart, 1, MPI_INTEGER, src, 101, MPI_COMM_WORLD, status, ierr)
+          
+          ! 2. Allocate Buffer just for this chunk
+          allocate(temp_buf(r_nx, nz))
+          
+          ! 3. Define where to write in file
+          st3 = [ r_istart, 1, rec_out ]
+          ct3 = [ r_nx, nz, 1 ]
+
+          ! 4. Recv & Write DENS
+          call MPI_Recv(temp_buf, r_nx*nz, MPI_DOUBLE_PRECISION, src, 200, MPI_COMM_WORLD, status, ierr)
+          call ncwrap(nf90_put_var(ncid, dens_varid, temp_buf, start=st3, count=ct3), __LINE__)
+
+          ! 5. Recv & Write UWND
+          call MPI_Recv(temp_buf, r_nx*nz, MPI_DOUBLE_PRECISION, src, 201, MPI_COMM_WORLD, status, ierr)
+          call ncwrap(nf90_put_var(ncid, uwnd_varid, temp_buf, start=st3, count=ct3), __LINE__)
+
+          ! 6. Recv & Write WWND
+          call MPI_Recv(temp_buf, r_nx*nz, MPI_DOUBLE_PRECISION, src, 202, MPI_COMM_WORLD, status, ierr)
+          call ncwrap(nf90_put_var(ncid, wwnd_varid, temp_buf, start=st3, count=ct3), __LINE__)
+
+          ! 7. Recv & Write THETA
+          call MPI_Recv(temp_buf, r_nx*nz, MPI_DOUBLE_PRECISION, src, 203, MPI_COMM_WORLD, status, ierr)
+          call ncwrap(nf90_put_var(ncid, theta_varid, temp_buf, start=st3, count=ct3), __LINE__)
+
+          deallocate(temp_buf)
+       end do
+
+    else
+       ! --- WORKER: Send Data to Rank 0 ---
+       call MPI_Send(nx, 1, MPI_INTEGER, 0, 100, MPI_COMM_WORLD, ierr)
+       call MPI_Send(i_start_global, 1, MPI_INTEGER, 0, 101, MPI_COMM_WORLD, ierr)
+       
+       call MPI_Send(dens, nx*nz, MPI_DOUBLE_PRECISION, 0, 200, MPI_COMM_WORLD, ierr)
+       call MPI_Send(uwnd, nx*nz, MPI_DOUBLE_PRECISION, 0, 201, MPI_COMM_WORLD, ierr)
+       call MPI_Send(wwnd, nx*nz, MPI_DOUBLE_PRECISION, 0, 202, MPI_COMM_WORLD, ierr)
+       call MPI_Send(theta, nx*nz, MPI_DOUBLE_PRECISION, 0, 203, MPI_COMM_WORLD, ierr)
     end if
 
     rec_out = rec_out + 1
@@ -152,37 +151,24 @@ module module_output
 
   subroutine close_output
     implicit none
-    ! Deallocate Local
+    
     if ( allocated(dens) ) deallocate(dens, uwnd, wwnd, theta)
     
     if (myrank == 0) then
-      ! Deallocate Global
-      if ( allocated(global_dens) ) deallocate(global_dens, global_uwnd, global_wwnd, global_theta)
       call ncwrap(nf90_close(ncid), __LINE__)
     end if
-    if ( allocated(recvcounts) ) then
-      deallocate(recvcounts)
-      deallocate(displs)
-    end if
-    if (myrank == 0) then
-      if ( allocated(global_dens) ) then
-        deallocate(global_dens)
-        deallocate(global_uwnd)
-        deallocate(global_wwnd)
-        deallocate(global_theta)
-      end if
-      call ncwrap(nf90_close(ncid), __LINE__)
-    end if
+    
   end subroutine close_output
 
   subroutine ncwrap(ierr,line)
     implicit none
     integer, intent(in) :: ierr
     integer, intent(in) :: line
+    integer :: mpi_err
     if (ierr /= nf90_noerr) then
       write(stderr,*) 'NetCDF Error (Rank ', myrank, ') at line: ', line
       write(stderr,*) nf90_strerror(ierr)
-      stop
+      call MPI_Abort(MPI_COMM_WORLD, -1, mpi_err)
     end if
   end subroutine ncwrap
 
